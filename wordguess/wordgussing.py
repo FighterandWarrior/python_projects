814b88db13a4c8f4810139302699c83e4b2bdf1f
Line <linehp@email.dk>
2026-04-04 15:47:34 +0200

    Create wordgussing.py

---
+++ wordguess/wordgussing.py
@@ -0,0 +1,8 @@
+#word guessing game
+import random
+print("Welcome to the Word Guessing Game!")
+
+words = ["home", "school", "python", "code", "computer", "game", "projects"]
+
+print("The word is: " + (random.choice(words)))
+
